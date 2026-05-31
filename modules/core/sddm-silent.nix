37cdb68230a810c09ff9627b52d5620cdded74c3
wyn <173407133+xuwyn@users.noreply.github.com>
2026-05-31 11:56:48 -0300

    clean up sddm-silent.nix

--- modules/core/sddm-silent.nix
+++ modules/core/sddm-silent.nix
@@ -6,38 +6,6 @@
   ...
 }: {
   imports = [inputs.silentSDDM.nixosModules.default];
-  # workaround sddm/nvidia race condition
-  # systemd.services.restart-dm-on-boot = {
-  #   description = "Delayed restart of display manager on boot";
-  #   wantedBy = ["multi-user.target"];
-  #   after = ["display-manager.service"];
-  #
-  #   serviceConfig = {
-  #     Type = "oneshot";
-  #     RemainAfterExit = true;
-  #     ExecStart = "${pkgs.bash}/bin/bash -c 'sleep 10 && systemctl restart display-manager'";
-  #   };
-  # };
-
-  # systemd.services.display-manager = {
-  #   # after = ["dev-dri-card1.device"];
-  #   # wants = ["dev-dri-card1.device"];
-  #
-  #   # Allow up to 3 start attempts within 30 seconds
-  #   startLimitIntervalSec = 30;
-  #   startLimitBurst = 3;
-  #
-  #   serviceConfig = {
-  #     # forced delay
-  #     ExecStartPre = "${pkgs.coreutils}/bin/sleep 5";
-  #     # timeout duration
-  #     TimeoutStartSec = "10s";
-  #     # Restart if the service crashes, exits with an error, or times out
-  #     Restart = "always";
-  #     # Wait 2 seconds before attempting the restart (gives Nvidia time to breathe)
-  #     RestartSec = lib.mkForce "2s";
-  #   };
-  # };
 
   services.displayManager = {
     sddm = {
