c40498214235102db1f9496984853c3badb6eac0
wyn <173407133+xuwyn@users.noreply.github.com>
2026-06-03 15:30:24 -0300

    add more addons to firefox

--- modules/home/firefox.nix
+++ modules/home/firefox.nix
@@ -43,13 +43,18 @@ in {
       name = username;
       isDefault = true;
 
-      extensions.force = true;
+      extensions.force = true; # enable overwrite
       extensions.packages = with pkgs.nur.repos.rycee.firefox-addons;
         [
           ublock-origin
           old-reddit-redirect
           reddit-enhancement-suite
           tree-style-tab
+          don-t-fuck-with-paste
+          return-youtube-dislikes
+          youtube-nonstop
+          darkreader
+          ctrl-number-to-switch-tabs
         ]
         ++ (with customAddons; [
           # pywalfox
@@ -60,12 +65,21 @@ in {
         "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
 
         # Clean up Firefox Home Content (New Tab Page)
+        "browser.newtabpage.activity-stream.logowordmark.alwaysVisible" = false;
+        "browser.newtabpage.activity-stream.feeds.weatherfeed" = false;
         "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
         "browser.newtabpage.activity-stream.feeds.topsites" = false;
         "browser.newtabpage.activity-stream.showSponsored" = false;
         "browser.newtabpage.activity-stream.showSponsoredCheckboxes" = false;
         "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
         "browser.newtabpage.activity-stream.showWeather" = false;
+        "browser.newtabpage.activity-stream.showSearch" = false;
+        "browser.newtabpage.activity-stream.system.showWeather" = false;
+        "browser.newtabpage.activity-stream.widgets.system.weather.enabled" = false;
+        "browser.newtabpage.activity-stream.widgets.weather.enabled" = false;
+
+        # Resume previous browser session
+        "browser.startup.page" = 3;
       };
     };
   };
