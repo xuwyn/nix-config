32bbbcf45c5d6a52f63deff2142ccd667c65da63
wyn <173407133+xuwyn@users.noreply.github.com>
2026-06-08 23:10:19 -0300

    add variables for apricot

---
+++ hosts/apricot/variables.nix
@@ -0,0 +1,27 @@
+{
+  ### USER ENVIRONMENT & APPLICATIONS
+  # Git Identity
+  gitUsername = "wyn";
+  gitEmail = "173407133+suquynh@users.noreply.github.com";
+
+  # Shell Settings (core/user.nix)
+  # (Options: "zsh", "bash")
+  shell = "zsh";
+
+  # Default Applications
+  terminal = "kitty";
+
+  # Alternative Terminal Toggles
+  tmuxEnable = false;
+  alacrittyEnable = false;
+  weztermEnable = false;
+  ghosttyEnable = false;
+
+  # Development Text Editors
+  vscodeEnable = false;
+  helixEnable = false;
+  zedEnable = true;
+
+  # File Managers
+  yaziEnable = true;
+}
