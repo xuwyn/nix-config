c84f48ac8a7dddbded4beb65663ab711feb2a63f
wyn <173407133+xuwyn@users.noreply.github.com>
2026-06-13 18:45:09 -0300

    add terminal choices for Thunar (not tested)

--- modules/core/thunar.nix
+++ modules/core/thunar.nix
@@ -1,4 +1,22 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  host,
+  ...
+}: let
+  inherit (import ../../hosts/${host}/variables.nix) terminal;
+  openTerminal =
+    if terminal == "foot"
+    then "foot -D %f"
+    else if terminal == "kitty"
+    then "kitty -d %f"
+    else if terminal == "alacritty"
+    then "alacritty --working-directory %f"
+    else if terminal == "wezterm"
+    then "wezterm start --cwd %f"
+    else if terminal == "ghostty"
+    then "ghostty --working-directory=%f"
+    else throw "Unknown terminal type set in thunar.nix!";
+in {
   programs.thunar = {
     enable = true;
     plugins = [
@@ -9,4 +27,23 @@
   environment.systemPackages = with pkgs; [
     ffmpegthumbnailer # Need For Video / Image Preview
   ];
+
+  # Open chosen terminal
+  xdg.configFile."Thunar/uca.xml".text = ''
+    <?xml version="1.0" encoding="UTF-8"?>
+    <actions>
+    <action>
+        <icon>utilities-terminal</icon>
+        <name>Open Terminal Here</name>
+        <submenu></submenu>
+        <unique-id>1710575157271461-1</unique-id>
+        <command>${openTerminal}</command>
+        <description>Open the current directory in foot</description>
+        <range></range>
+        <patterns>*</patterns>
+        <startup-notify/>
+        <directories/>
+    </action>
+    </actions>
+  '';
 }
