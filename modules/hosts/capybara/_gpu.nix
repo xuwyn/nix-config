1960240ccf254b51645c3d3882e3a56faccd29b6
wyn <173407133+xuwyn@users.noreply.github.com>
2026-06-29 07:28:22 -0300

    chore(hosts): clean up _gpu fakeHash

--- modules/hosts/capybara/_gpu.nix
+++ modules/hosts/capybara/_gpu.nix
@@ -9,8 +9,6 @@
         nvidia = {
           enable = true;
           version = "610.43.02"; # CHANGE THIS AFTER DRIVER UPDATE!!!
-          # donot delete, dummy hash for nix to fetch the right one
-          # sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
           sha256 = "sha256-MDSgVLtM33dS/43CclZMsQVROAS/9TU4lFkBsWyndGM=";
         };
       };
