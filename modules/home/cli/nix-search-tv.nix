4e6aad2c51d32563b3a5a4fe8e73e041536981e4
wyn <173407133+xuwyn@users.noreply.github.com>
2026-07-04 11:41:50 -0300

    feat(cli): add nix-search-tv

---
+++ modules/home/cli/nix-search-tv.nix
@@ -0,0 +1,34 @@
+{
+  flake.modules.homeManager.cli = {
+    config,
+    lib,
+    pkgs,
+    ...
+  }: let
+    cfg = config.homeManager.cli.nix-search-tv;
+
+    # shell abbr. and fzf integration
+    ns-script = pkgs.writeShellApplication {
+      name = "ns";
+      runtimeInputs = with pkgs; [
+        fzf
+        nix-search-tv
+      ];
+      text = builtins.readFile "${pkgs.nix-search-tv.src}/nixpkgs.sh";
+    };
+  in {
+    options.homeManager.cli.nix-search-tv = {
+      enable = lib.mkEnableOption "Enable nix-search-tv with TV and fzf";
+    };
+    config = lib.mkIf cfg.enable {
+      # tv integration
+      programs.nix-search-tv = {
+        enable = true;
+        enableTelevisionIntegration = true;
+      };
+
+      # add to user path
+      home.packages = [ns-script];
+    };
+  };
+}
