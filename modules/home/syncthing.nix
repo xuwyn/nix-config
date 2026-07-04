64858fbc9f7d583170d163d03c89d4364ade8d0b
wyn <173407133+xuwyn@users.noreply.github.com>
2026-07-04 00:02:35 -0300

    chore: update syncthing
    - syncthing: force https on web gui
    - sops: change syncthing password
    - docs: remove nh switch -b bak from readme.md

--- modules/home/syncthing.nix
+++ modules/home/syncthing.nix
@@ -12,14 +12,15 @@
         username = username;
         passwordFile = config.sops.secrets.syncthing_password.path;
       };
+      guiAddress = "https://127.0.0.1:8384";
       overrideFolders = true;
       settings = {
+        urAccepted = -1; # do not submit anonymous usage data
         folders = {
-          "shared-documents" = {
-            id = "documents-sync-id";
+          "Shared" = {
+            id = "e4820aa7e79671fc";
             label = "Shared";
             path = "${config.home.homeDirectory}/Shared";
-            devices = [];
             ignorePerms = true;
           };
         };
