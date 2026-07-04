2b04caece36d3bdd4665da5f6b7bcb481f0de829
wyn <173407133+xuwyn@users.noreply.github.com>
2026-07-04 13:05:46 -0300

    feat(cli): add television

---
+++ modules/home/cli/television.nix
@@ -0,0 +1,30 @@
+{
+  flake.modules.homeManager.cli = {
+    config,
+    lib,
+    ...
+  }: let
+    cfg = config.homeManager.cli.television;
+  in {
+    options.homeManager.cli.television = {
+      enable = lib.mkEnableOption "Enable television";
+    };
+    config = lib.mkIf cfg.enable {
+      programs.television = {
+        enable = true;
+        enableBashIntegration = true;
+        enableZshIntegration = true;
+        enableFishIntegration = true;
+        enableNushellIntegration = true;
+        settings = {
+          tick_rate = 50;
+          ui = {
+            use_nerd_font_icons = true;
+            ui_scale = 120;
+            show_preview_panel = true;
+          };
+        };
+      };
+    };
+  };
+}
