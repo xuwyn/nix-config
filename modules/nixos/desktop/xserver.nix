300ba425ac661a43eb527a9416d794f5a2dc8dbd
wyn <173407133+xuwyn@users.noreply.github.com>
2026-07-20 09:57:07 -0300

    refactor: drop keyboard layout options

--- modules/nixos/desktop/xserver.nix
+++ modules/nixos/desktop/xserver.nix
@@ -8,49 +8,13 @@
     cfg = config.nixos.desktop.xserver;
   in {
     options.nixos.desktop.xserver = {
-      enable = lib.mkEnableOption "Enable xserver for keyboard layout";
-      keyboardLayout = lib.mkOption {
-        type = lib.types.str;
-        default = "us";
-        description = "Keyboard layout (e.g., us, dvorak, fr)";
-      };
-      keyboardVariant = lib.mkOption {
-        type = lib.types.str;
-        default = "";
-        description = "Keyboard layout variant (e.g., intl, colemak)";
+      enable = lib.mkEnableOption "Enable xserver";
+    };
+    config = lib.mkIf cfg.enable {
+      services.xserver = {
+        enable = true;
+        excludePackages = [pkgs.xterm];
       };
     };
-
-    config = let
-      usVariants = ["dvorak" "colemak" "workman" "intl" "us-intl" "altgr-intl"];
-      normalize = v:
-        if v == "us-intl"
-        then "intl"
-        else v;
-
-      isUSVariant = builtins.elem cfg.keyboardLayout usVariants || builtins.elem cfg.keyboardVariant usVariants;
-
-      finalKbLayout =
-        if isUSVariant
-        then "us"
-        else cfg.keyboardLayout;
-
-      finalKbVariant =
-        if builtins.elem cfg.keyboardVariant usVariants
-        then normalize cfg.keyboardVariant
-        else if builtins.elem cfg.keyboardLayout usVariants
-        then normalize cfg.keyboardLayout
-        else cfg.keyboardVariant;
-    in
-      lib.mkIf cfg.enable {
-        services.xserver = {
-          enable = true;
-          excludePackages = [pkgs.xterm];
-          xkb = {
-            layout = finalKbLayout;
-            variant = finalKbVariant;
-          };
-        };
-      };
   };
 }
