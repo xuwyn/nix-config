4a73630930c6582f2fab7677b8b80891c5fe39f2
wyn <173407133+xuwyn@users.noreply.github.com>
2026-07-25 21:59:39 -0300

    chore: drop unnecessary pkgs in hyprland and niri

--- modules/home/niri/_packages.nix
+++ modules/home/niri/_packages.nix
@@ -1,8 +1,6 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
     libnotify # Linux notification tool (provides notify-send)
-    playerctl # Controls Linux media keys via D-Bus
-    socat # Network utility (used here for Wayland screenshots)
     wl-clipboard # Wayland clipboard
     cliphist # Clipboard history engine
   ];
