1df26ba110173a23572adaddbb359c56d7522942
wyn <173407133+xuwyn@users.noreply.github.com>
2026-08-14 16:42:46 -0300

    chore(hosts): update capybara nvidia driver
    - docs: update hyprland screenshot

--- modules/hosts/capybara/_gpu.nix
+++ modules/hosts/capybara/_gpu.nix
@@ -8,8 +8,8 @@
         enable = true;
         nvidia = {
           enable = true;
-          version = "610.43.03"; # CHANGE THIS AFTER DRIVER UPDATE!!!
-          sha256 = "sha256-ReLUwTSiPDXlDyU6SqY+fl6NF+PRhdSgfIpY6WEu05I=";
+          version = "610.57.04"; # CHANGE THIS AFTER DRIVER UPDATE!!!
+          sha256 = "sha256-suk1xmuDuwDAyFe8jg7g/VLekoa0DJzB7sKafOfrEW0=";
         };
       };
     };
