26c2df7a7160948277cfb0b2b6c5e59ce2f477a6
wyn <173407133+xuwyn@users.noreply.github.com>
2026-08-20 22:00:07 -0300

    feat(rpi5): add fileSystems

--- modules/nixos/drivers/rpi5.nix
+++ modules/nixos/drivers/rpi5.nix
@@ -14,7 +14,16 @@
       imports = [inputs.nixos-hardware.nixosModules.raspberry-pi-5];
       config = mkIf cfg.enable {
         nix.settings.system-features = ["nixos-test" "benchmark" "big-parallel" "kvm" "gccarch-armv8-a"];
-
+        fileSystems = {
+          "/" = {
+            device = "/dev/disk/by-label/NIXOS_SD";
+            fsType = "ext4";
+          };
+          "/boot/firmware" = {
+            device = "/dev/disk/by-label/FIRMWARE";
+            fsType = "vfat";
+          };
+        };
         # Read: https://wiki.nixos.org/wiki/NixOS_on_ARM/Raspberry_Pi
         hardware.raspberry-pi = {
           firmware = {
