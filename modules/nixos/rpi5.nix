b7423078fa4b99affd08b1b144bc9450e4fd8174
wyn <173407133+xuwyn@users.noreply.github.com>
2026-08-21 20:42:35 -0300

    refactor(rpi5): add options to build custom sd image (wip)

--- modules/nixos/rpi5.nix
+++ modules/nixos/rpi5.nix
@@ -3,11 +3,16 @@
     lib,
     config,
     inputs,
+    pkgs,
     ...
   }: {
-    imports = [inputs.nixos-hardware.nixosModules.raspberry-pi-5];
+    imports = [
+      inputs.nixos-hardware.nixosModules.raspberry-pi-5
+      "${inputs.nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
+    ];
     boot.initrd.systemd.enable = lib.mkForce false;
-    nix.settings.system-features = ["nixos-test" "benchmark" "big-parallel" "kvm" "gccarch-armv8-a"];
+    boot.supportedFilesystems.zfs = lib.mkForce false;
+    sdImage.firmwareSize = 512; # MB
     fileSystems = {
       "/" = {
         device = "/dev/disk/by-label/NIXOS_SD";
@@ -18,25 +23,20 @@
         fsType = "vfat";
       };
     };
+    nix.settings.system-features = ["nixos-test" "benchmark" "big-parallel" "kvm" "gccarch-armv8-a"];
     # Read: https://wiki.nixos.org/wiki/NixOS_on_ARM/Raspberry_Pi
     hardware.raspberry-pi = {
       firmware = {
         enable = true;
-        # uboot.enable = true;
+        uboot.enable = true;
       };
-      # configtxt.settings = {
-      #   all = {
-      #     dtparam = [
-      #       "audio=on"
-      #       "i2c_arm=on"
-      #     ];
-      #     dtoverlay = [
-      #       "vc4-kms-v3d"
-      #       # "disable-bt"
-      #     ];
-      #     arm_boost = lib.mkForce null;
-      #   };
-      # };
+      configtxt.settings = {
+        all = {
+          dtparam = ["audio=on" "i2c_arm=on"];
+          dtoverlay = ["vc4-kms-v3d"];
+          arm_boost = lib.mkForce null;
+        };
+      };
     };
   };
 }
