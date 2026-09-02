3fc0c5f8602d1a2bf1867b65b10f4c513d93e184
wyn <173407133+xuwyn@users.noreply.github.com>
2026-09-02 05:10:58 -0300

    refactor(flake): change to extra-* sub, drop cache.nixos

--- flake.nix
+++ flake.nix
@@ -1,16 +1,14 @@
 {
   # Binary caches
   nixConfig = {
-    substituters = [
-      "https://cache.nixos.org"
+    extra-substituters = [
       "https://nix-community.cachix.org"
       "https://hyprland.cachix.org"
       "https://noctalia.cachix.org"
       "https://cache.xinux.uz"
       "https://nixos-raspberrypi.cachix.org"
     ];
-    trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
